c331ac5da6e3f5e32dcf7bade8c266d92183833a
Rafus71 <rulanski71@gmail.com>
2019-10-20 00:18:50 +0100

    Test use  css transform by tachyons

--- src/Hello.js
+++ src/Hello.js
@@ -6,7 +6,7 @@ import './Hello.css';
 class Hello extends Component {
     render() {
         return ( <
-            div >
+            div class = 'f1 tc' >
             return <h1 > Hello Word < /h1> <
             p > Welcome to React < /p> < /
             div >
