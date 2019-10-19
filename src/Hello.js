5e457c37e5e341132a401385982227710b8139d3
Rafus71 <rulanski71@gmail.com>
2019-10-19 21:36:46 +0100

    Test  syntax multicomponents return propety in Hello.js create for test Hello.css file

--- src/Hello.js
+++ src/Hello.js
@@ -1,10 +1,16 @@
 import React, {
     Component
 } from "react";
+import './Hello.css';
 
 class Hello extends Component {
     render() {
-        return <h1 > Hello Word < /h1>
+        return ( <
+            div >
+            return <h1 > Hello Word < /h1> <
+            p > Welcome to React < /p> < /
+            div >
+        );
     }
 }
 
