941e3eb3ddce42ff01b64bd7112a099943bd4300
Rafus71 <rulanski71@gmail.com>
2019-10-20 13:06:43 +0100

    add  Tachoyons styles to card

--- src/Card.js
+++ src/Card.js
@@ -1,15 +1,19 @@
 import React from "react";
 
 const Card = () => {
-  return (
-    <div>
-      <img alt="robots" src="https://robohash.org/test?200x200" />
-      <div>
-        <h2>Jane Doe</h2>
-        <p>jane.doe@mail.com</p>
-      </div>
-    </div>
-  );
+    return ( <
+        div className = 'bg-light-green dib br3 pa3 ma2 grow' >
+        <
+        img alt = "robots"
+        src = "https://robohash.org/test?200x200" / >
+        <
+        div >
+        <
+        h2 > Jane Doe < /h2> <
+        p > jane.doe @mail.com < /p> <
+        /div> <
+        /div>
+    );
 };
 
 export default Card;
